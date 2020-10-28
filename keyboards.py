300e26410f968fd82be350dda052809f9ae2449a
Sangroyal <sangroyaled@gmail.com>
2020-10-28 14:11:02 +0300

    Improved the code style

--- keyboards.py
+++ keyboards.py
@@ -11,22 +11,26 @@ send_contact_btn = KeyboardButton('Отправить свой контакт �
 send_location_btn = KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
 
 """Структура выпадающей клавиатуры"""
-screen_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True). \
-    add(hi_btn).add(menu_btn).add(send_contact_btn).add(send_location_btn)
+screen_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)\
+    .add(hi_btn)\
+    .add(menu_btn)\
+    .add(send_contact_btn)\
+    .add(send_location_btn)\
 
 """Конструкция используемая для скрытия выпадающей клавиатуры"""
 remove_kb = ReplyKeyboardRemove()
 
 """Кнопки инлайн-клавиатуры"""
-get_plan_inline_btn = InlineKeyboardButton('🗓Получить план тренировки на сегодня🗓', callback_data='start_training')
-today_inline_btn = InlineKeyboardButton('💪Поднял сегодня💪', callback_data='today')
-last_workout_inline_btn = InlineKeyboardButton('🏁Последняя тренировка🏁', callback_data='last_training')
-month_inline_btn = InlineKeyboardButton('🌜Статистика за месяц🌜', callback_data='month')
-instagram_inline_btn = InlineKeyboardButton('🥇Instagram лучшего тренера🥇', url='https://www.instagram.com/oleg.akifjev/')
-get_categories_inline_btn = InlineKeyboardButton('Справочник упражнений', callback_data='categories')
+get_plan_inline_btn = InlineKeyboardButton('Получить план тренировки на сегодня🗓', callback_data='start_training')
+today_inline_btn = InlineKeyboardButton('Поднял сегодня💪', callback_data='today')
+last_workout_inline_btn = InlineKeyboardButton('Последние записи🏁', callback_data='last_training')
+month_inline_btn = InlineKeyboardButton('Статистика за месяц🌜', callback_data='month')
+instagram_inline_btn = InlineKeyboardButton('Instagram🥇', url='https://www.instagram.com/oleg.akifjev/')
+get_categories_inline_btn = InlineKeyboardButton('Справочник упражнений🔹', callback_data='categories')
 
 """Структура инлан-клавиатуры"""
-inline_kb_full = InlineKeyboardMarkup(row_width=2).add(get_plan_inline_btn).\
-            add(today_inline_btn, last_workout_inline_btn, month_inline_btn).\
-            add(instagram_inline_btn).add(get_categories_inline_btn)
+inline_kb_full = InlineKeyboardMarkup(row_width=2)\
+    .add(get_plan_inline_btn)\
+    .add(today_inline_btn, last_workout_inline_btn, month_inline_btn)\
+    .add(instagram_inline_btn).add(get_categories_inline_btn)
 
