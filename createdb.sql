4f2be7db134bfd6fbc3d45e91d09ba309cb559da
Sangroyal <sangroyaled@gmail.com>
2020-10-28 14:09:51 +0300

    Improved the code style and added new entity tables to implement new functionality

--- createdb.sql
+++ createdb.sql
@@ -1,32 +1,62 @@
-create table training_plan(
-    codename varchar(255) primary key,
-    user_id integer,
-    daily_volume integer
+CREATE TABLE training_plan(
+    codename VARCHAR(255) PRIMARY KEY,
+    user_id INTEGER,
+    daily_volume INTEGER
 );
 
-create table category(
-    codename varchar(255) primary key,
-    name varchar(255),
+INSERT INTO training_plan(codename, daily_volume) VALUES ('base', 2000);
+
+CREATE TABLE category(
+    codename VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
     is_base_exercise boolean,
     aliases text
 );
 
-create table exercises(
-    id integer primary key,
-    user_id integer,
-    weight integer,
-    reiteration integer,
+INSERT INTO category (codename, name, is_base_exercise, aliases)
+VALUES
+    ("bench press", "жим", TRUE, "жим штанги лежа, жим, жим лежа"),
+    ("squats", "приседание со штангой", TRUE, "присед, приседания"),
+    ("deadlift", "становая тяга", TRUE, "становая, тяга"),
+    ("other", "прочее", FALSE, "");
+
+CREATE TABLE exercises(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    weight INTEGER,
+    repetitions INTEGER,
     created datetime,
-    category_codename integer,
-    raw_text text,
+    category_codename INTEGER,
+    name TEXT
+    record_id INTEGER,
     FOREIGN KEY(category_codename) REFERENCES category(codename)
 );
 
-insert into category (codename, name, is_base_exercise, aliases)
-values
-    ("bench press", "жим", true, "жим штанги лежа, жим, жим лежа"),
-    ("squats", "приседание со штангой", true, "присед, приседания"),
-    ("deadlift", "становая тяга", true, "становая, тяга"),
-    ("other", "прочее", false, "");
+--CREATE TABLE individual_plan(
+--    user_id INTEGER PRIMARY KEY,
+--    training_id INTEGER,
+--    week_day TEXT,
+--    repetition_id INTEGER,
+--    weight INTEGER,
+--    numb_of_reps INTEGER,
+--    exercise_name TEXT
+--    start_date,
+--);
+--
+--CREATE TABLE users(
+--    user_id INTEGER PRIMARY KEY,
+--    name text,
+--    sex INTEGER,
+--    t_number VARCHAR(12),
+--    weight INTEGER,
+--    age INTEGER,
+--    individual_plan_status text,
+--    orders_count INTEGER,
+--);
 
-insert into training_plan(codename, daily_volume) values ('base', 2000);
+
+
+     )
+
+
+
